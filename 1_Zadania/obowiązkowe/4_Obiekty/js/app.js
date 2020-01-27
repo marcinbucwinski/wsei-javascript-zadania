7962173ef31abcd88d1608eae97d859e3e150a04
marcinbucwinski <43888277+marcinbucwinski@users.noreply.github.com>
2020-01-27 20:01:13 +0100

    Update app.js

--- 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
+++ 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
@@ -16,6 +16,26 @@ console.log(ulubionyPrzepis.title);
 console.log(ulubionyPrzepis.servings);
 console.log(ulubionyPrzepis.ingredients);
 
+//Zadanie 4
+var movie = {
+    director: "Peter Jackson",
+    writers: [" J.R.R. Tolkien ", "Fran Walsh", "Philippa Boyens", "Peter Jackson"],
+    stars: ["Elijah Wood", "Ian McKellen", "Orlando Bloom"],
+    country: "New Zealand | USA",
+    budget: "$93,000,000",
+    imdbPoints: 8.8,
+    year: 2001,
+    time: "2h 58 min",
+    category: " Adventure, Drama, Fantasy"
+};
+
+for (x in movie){
+    console.log(movie[x])
+};
+
+
+
+
 //Zadanie 6
 var spoon = {
     isExist: true
