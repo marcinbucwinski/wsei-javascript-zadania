e3c921e2f71c316cf91a239b7542ab5a70150631
marcinbucwinski <43888277+marcinbucwinski@users.noreply.github.com>
2020-01-27 11:39:13 +0100

    Update app.js

---
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -0,0 +1,13 @@
+
+
+//Zadanie 4
+var array1=[1,2,3,4,5,6,7];
+function multiply(array1) {
+    var temp=1;
+    for (let i=0; i<array1.length; i++){
+    temp*=array1[i];
+    };
+console.log(temp)};
+console.log(multiply(array1));
+
+//Zadanie 5
