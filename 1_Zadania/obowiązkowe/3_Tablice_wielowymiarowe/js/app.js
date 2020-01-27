e2f79964fe1157bce70908b5e8d72c4193839d7b
marcinbucwinski <43888277+marcinbucwinski@users.noreply.github.com>
2020-01-27 15:45:43 +0100

    Update app.js

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -13,3 +13,43 @@ var task2Array = [
     [5, 6, 7, 8],
     [9, 10, 11, 12]
 ];
+
+//Zadanie 1
+console.log(task1Array[3][2]);
+console.log(task1Array[1].length);
+console.log(task1Array[4][2]);
+
+
+//Zadanie 3
+var arr3 = [
+    [11, 12],
+    [42, 2],
+    [-4, -120],
+    [0, 2],
+    [1, 34],
+    ];
+function print2DArray(array){
+    for(var i=0; i<array.length; i++){
+        for (var j=0; j<array[1].length; j++){
+            console.log(array[i][j]);
+        }
+    }
+};
+console.log(print2DArray(arr3));
+
+// Zadanie 4
+var arr3 = [
+    [-78, 25],
+    [22, 26],
+    [5, 80],
+    [-63, 2],
+    
+    ];
+function print2DArray(array){
+    for(var i=0; i<array.length; i++){
+        for (var j=0; j<array[1].length; j++){
+            console.log(array[i][j]);
+        }
+    }
+};
+console.log(print2DArray(arr3));
