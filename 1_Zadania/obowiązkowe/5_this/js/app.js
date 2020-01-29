b062dadcc1a1ba327fcd8317f2b0414d71adc98d
marcinbucwinski <43888277+marcinbucwinski@users.noreply.github.com>
2020-01-29 20:46:28 +0100

    Update app.js

--- 1_Zadania/obowiązkowe/5_this/js/app.js
+++ 1_Zadania/obowiązkowe/5_this/js/app.js
@@ -6,14 +6,28 @@ var car={
     printCarinfo:function(){
         console.log(this.brand,'', this.color,'', this.numberOfKilometers);
     },
-    drive:function(a){
-         this.numberOfKilometers+=a;
+    drive:function(kilometers){
+         this.numberOfKilometers+=kilometers;
     }
     };
 
 console.log(car.printCarinfo()); 
 console.log(car.drive(20));
 console.log(car.printCarinfo()); 
+
+//Zadanie 1
+pokazDatyPrzegladow:function(){
+        console.log(this.datyPrzegladow);
+    },
+    dodajDatePrzegladu:function(data){
+        this.datyPrzegladow.push(data);
+    }
+    };
+
+car1.datyPrzegladow=["12.10.2016","15.10.2017","14.10.2018"]; 
+console.log(car1.pokazDatyPrzegladow())
+console.log(car1.dodajDatePrzegladu("14.01.2019"));
+console.log(car1.pokazDatyPrzegladow())
 
 //Zadanie 3
 var stairs={
