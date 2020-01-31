1a1f8f1ed8930b9eb50ebc3eb40c26fe969045cd
marcinbucwinski <43888277+marcinbucwinski@users.noreply.github.com>
2020-01-31 14:13:35 +0100

    Update zadanie01.js

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie01.js
@@ -1,5 +1,24 @@
-var menu = document.querySelector("#menu");
+/*var menu = document.querySelector("#menu");
 var paragraf = document.querySelector("p");
 
 menu.classList.add("menu");
-paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+paragraf.innerHTML = "A to jest paragraf w zadaniu 1";*/
+
+/* W przypadku umieszczenia skryptu w nagłówku strony, 
+skrypt zostanie wywołany kiedy nie nastąpiło jeszcze wczytanie DOM.
+Natomiast umieszczenie skryptu na dole strony spowoduje 
+jego uruchomienie po załadowaniu DOM.*/
+
+document.addEventListener("DOMContentLoaded",function(){
+    var menu = document.querySelector("#menu");
+    var paragraf = document.querySelector("p");
+    
+    menu.classList.add("menu");
+    paragraf.innerHTML = "A to jest paragraf w zadaniu 1";
+});
+
+/*Stworzenie eventu sprawiło, że kod jest wykonywany 
+dopiero po załadowaniu strony. */
+
+/*Po przeniesieniu skryptu na dół strony event uruchomi go tak samo. 
+Dopiero po załadowaniu strony. Nie ma różnicy w wykonywaniu skryptu. */  
