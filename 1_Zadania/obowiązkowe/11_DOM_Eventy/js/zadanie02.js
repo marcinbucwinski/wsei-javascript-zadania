8f3b138901e4a900948bcc2aa1ed03a2f8891a0c
marcinbucwinski <43888277+marcinbucwinski@users.noreply.github.com>
2020-01-31 14:15:00 +0100

    Update zadanie02.js

---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie02.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("mainBtn").addEventListener("click",function(){
+        console.log("Hura! Działa!");
+    })
+});
